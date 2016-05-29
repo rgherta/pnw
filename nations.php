d49b065b81227b2ce382a2d4783d6bda09f4f6d6
Roman Gherta <rgherta@users.noreply.github.com>
2016-05-29 19:05:52 +0300

    Update nations.php

--- nations.php
+++ nations.php
@@ -1,19 +1,51 @@
 <?php
 
 libxml_use_internal_errors(true);
-header("Content-type: text/plain");
+header("content-type: application/force-download");
+header('Content-Type: application/csv');
+header('Pragma: no-cache');
 
 $alliance = $_GET['alliance'];
+$result = "";
 
 $json = file_get_contents('https://politicsandwar.com/api/nations/');
+
 $nations= json_decode($json);
 $nation = $nations->nations;
 
 foreach ($nation as $nt) {
     
     if (strtoupper($nt->alliance) === strtoupper ($alliance)) {
-    echo $nt->nationid . "\r\n";
+    $result = $result . GetNationStats($nt->nationid) ;
     }
 }
 
+$result = "[" . substr_replace($result, "", strrpos($result, ",")) . "]" ;
+
+
+$resultarray = json_decode($result, true);
+
+$fp = fopen('php://output', 'a');
+fputcsv($fp, array_keys($resultarray[0]));
+
+foreach ($resultarray as $row) {
+    fputcsv($fp, $row);
+}
+fclose($fp);
+
+
+//Functions
+
+function GetNationStats($nationid) {
+    
+    $json = file_get_contents('https://politicsandwar.com/api/nation/id=' . $nationid);
+    
+    return $json . ", ";
+
+    $json = null;
+    unset($json);
+    
+}
+
 ?>
+
