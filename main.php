c6851874432ca01d8b2597e2904da89593943386
Roman Gherta <rgherta@users.noreply.github.com>
2016-05-29 13:29:46 +0300

    Update main.php

--- main.php
+++ main.php
@@ -3,16 +3,38 @@ libxml_use_internal_errors(true);
 
 $alliance = $_GET['alliance'];
 $file = "https://www.politicsandwar.com/index.php?id=15&memberview=true&keyword=" . $alliance . "&cat=alliance&ob=score&od=DESC&backpage=%3C%3C&maximum=500&minimum=50" ;
-$doc = new \DOMDocument();
+$doc = new DOMDocument();
 $doc->loadHTMLFile($file);
-
 $xpath = new DOMXpath($doc);
  
 $nodes = $xpath->query("//table[@class='nationtable']//a[contains(@href, 'nation')]//@href");
 header("Content-type: text/plain");
 
 foreach($nodes as $node) {
- echo $node->nodeValue . "\r\n" ;
+ GetDataPnW($node->nodeValue);
+}
+
+
+
+// Functions
+
+function GetDataPnW($url) {
+    $doctemp = new DOMDocument();
+    $doctemp->loadHTMLFile($url);
+    $xpathtemp = new DOMXpath($doctemp);
+    
+    $nodestemp = $xpathtemp->query("//*[@id='rightcolumn']/div[8]/div[1]/table/tbody/tr[35]/td[2]/text()");
+    
+    foreach($nodestemp as $nodetemp) {
+        echo $nodetemp->nodeValue . "\r\n" ;
+    }
+    
+    $doctemp = null;
+    $nodestemp = null;
+    $xpathtemp = null;
+    unset($doctemp);
+    unset($nodestemp);
+    unset($xpathtemp);
 }
 
 ?>
