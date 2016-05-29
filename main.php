ea2c850d0512b51f9a822a49c3ef7af4d6fdafbb
Roman Gherta <rgherta@users.noreply.github.com>
2016-05-29 14:24:22 +0300

    Update main.php
    Updated with Nationid

--- main.php
+++ main.php
@@ -19,22 +19,34 @@ foreach($nodes as $node) {
 // Functions
 
 function GetDataPnW($url) {
+    
+    //take nationid from url
+    $nationid = substr($url, strripos($url,"=")+1, strlen($url)-strripos($url,"="));
+    
+    //retrieve DOM data
     $doctemp = new DOMDocument();
     $doctemp->loadHTMLFile($url);
     $xpathtemp = new DOMXpath($doctemp);
     
-    $nodestemp = $xpathtemp->query("//*[@id='rightcolumn']/div[8]/div[1]/table/tbody/tr[35]/td[2]/text()");
+    $nodestemp = $xpathtemp->query("//div[@class='col-sm-6 col-xs-12']/table[@class='nationtable']/tr/td[contains(text(),'Ships:')]//following-sibling::td/text()");
     
     foreach($nodestemp as $nodetemp) {
-        echo $nodetemp->nodeValue . "\r\n" ;
+        if ($nodetemp->nodeValue !== "0") {
+            echo "NationId " . $nationid . " currently has " . $nodetemp->nodeValue . " ships \r\n";
+        }
     }
     
+    //release memory
     $doctemp = null;
     $nodestemp = null;
     $xpathtemp = null;
     unset($doctemp);
     unset($nodestemp);
     unset($xpathtemp);
+    usleep(500);
 }
+
+
+
 
 ?>
